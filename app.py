cc31729e26fdcb3ef95162fe3dc7779005b4086a
FahadMehmood123 <faddy.xl123@gmail.com>
2022-10-16 11:15:59 -0700

    Flask web app for FYP

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask,render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+
+
 
 
 app=Flask(__name__)
+db=SQLAlchemy(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///database.db'
+
 
 @app.route('/')
-
-
-
-
-
 def index():
     return render_template('index.html')
+
 
 if __name__=="__main__":
     app.run(debug=True)
