e708178bc95f68191ba856c593421e1918367bb5
FahadMehmood123 <faddy.xl123@gmail.com>
2022-10-16 11:19:05 -0700

    SQLite Connectivity

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
-from flask import Flask,render_template,url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,flash,render_template,url_for,request,redirect
+import sqlite3
+
+
+connection=sqlite3.connect('database.db')
+cursor=connection.cursor()
 
 
 
+command2="""CREATE TABLE IF NOT EXISTS User(TeacherId INTEGER,Password TEXT)"""
+cursor.execute(command2)
 
+comd="""SELECT * FROM User"""
+cursor.execute(comd)
 
+connection.commit()
 
 
 app=Flask(__name__)
-db=SQLAlchemy(app)
+app.secret_key="__privatekey__"
 
-app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///database.db'
-
-
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html') 
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    if request.method=='POST':
+        connection=sqlite3.connect('database.db')
+        cursor=connection.cursor()
+        name=request.form['AdminKey']
+        TeacherID=request.form['TeacherId']
+        Password=request.form['Password']
+        if name=="12345":
+            cursor.execute("INSERT INTO User(TeacherId,Password) VALUE(?,?)",(TeacherID,Password))
+            connection.commit()
+        else:
+            flash("Invalid Admin Key")
+    return render_template('signup.html') 
+
 
 
 if __name__=="__main__":
